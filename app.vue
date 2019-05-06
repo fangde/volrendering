af4b37438bc5b3b52478f037298d4fb3bab42ac9
fliu <fangde.liu@outlook.com>
2019-05-06 22:24:02 +0800

    add vue

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+<canvas>
+
+</canvas>
+</template>
+<script>
+import {GPU} from 'gpu.js'
+console.log(GPU)
+
+const gpu=new GPU();
+const settings={
+    output:{x:128}
+}
+const kernel=gpu.createKernel(
+    function(){
+        return this.thread.x+10.0;
+    },settings
+)
+
+console.log(kernel())
+
+export default {
+    
+}
+</script>
+<style>
+
+</style>
